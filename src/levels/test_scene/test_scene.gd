8eda68f91dc6a8c0510901f05864024ebd12f24d
AnidemDex <dav.medrin@gmail.com>
2021-01-02 23:31:16 -0500

    added a request_text signal
    to popup the angela's mind

--- src/levels/test_scene/test_scene.gd
+++ src/levels/test_scene/test_scene.gd
@@ -4,3 +4,7 @@ onready var narrator = $CanvasLayer/Narrator
 
 func _on_Angela2_died():
 	narrator.set_text_n_show("Angela probably died")
+
+
+func _on_Angela_request_text(text):
+	narrator.set_text_n_show(text)
--- src/player/angela.gd
+++ src/player/angela.gd
@@ -1,6 +1,7 @@
 extends PlayableBody2D
 
 signal died
+signal request_text(text)
 
 onready var hurtbox:Area2D = $CollisionsAreas/HurtBox
 onready var hitbox:Area2D = $CollisionsAreas/HitBox
@@ -10,6 +11,7 @@ func die() -> void:
 	# Replace with the real death
 	print("Angela probably died, but i'm not going to remove it from scene")
 	emit_signal("died")
+	emit_signal("request_text", "Now Angela is dead")
 
 func _on_Stats_no_health() -> void:
 	die()
@@ -17,5 +19,26 @@ func _on_Stats_no_health() -> void:
 
 func _on_HurtBox_body_entered(body:Node)->void:
 	# This can be a group or a generic class (Enemy)
-	if body.is_in_group("enemies"):
-		stats.health -= body.damage
+	stats.health -= body.damage
+
+
+func _on_Stats_health_changed(value):
+	# FIXME: Another implementation pls
+	
+	var msg = ""
+	if stats:
+		match stats.health:
+			stats.max_health:
+				msg = "Angela needs money"
+			5:
+				msg = "Still no money, Angela starts struggling to think straight"
+			3:
+				msg = "Angela is really struggling to think straight"
+			2:
+				msg = "The harshness of reality is starting to set in, angela is horrified that her mom may pass. "
+			1:
+				msg = "Angela has a mental breakdown. she was so close, she failed. her mom never recovered"
+			_:
+				pass
+		if msg:
+			emit_signal("request_text", msg)
