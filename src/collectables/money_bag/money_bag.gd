5cb685974b555792b27070efd061356ceca4853d
landobot27 <46508791+landobot27@users.noreply.github.com>
2021-01-04 16:42:10 -0500

    enemies, player, health, and sanity hotfix 1
    i have started fixing enemies, it appears that all of my health and sanity code has somehow been lost, for a 2nd or 3rd time, huh.

--- src/collectables/money_bag/money_bag.gd
+++ src/collectables/money_bag/money_bag.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+var damage = 1
 
 func _on_MoneyBag_body_entered(body):
 	queue_free()
--- src/player/angela.gd
+++ src/player/angela.gd
@@ -6,9 +6,6 @@ signal request_text(text)
 onready var hurtbox:Area2D = $CollisionsAreas/HurtBox
 onready var stats:Node = $Stats
 
-func _ready():
-	self.PAUSE_MODE_STOP
-
 func die() -> void:
 	# Replace with the real death
 	print("Angela probably died, but i'm not going to remove it from scene")
@@ -22,12 +19,11 @@ func _on_Stats_no_health() -> void:
 func _on_HurtBox_body_entered(body:Node)->void:
 	# This can be a group or a generic class (Enemy)
 	if body.is_in_group("enemies"):
-		stats.health -= body.damage
+		stats.sanity -= body.damage
 
 
 func _on_Stats_health_changed(value):
-	# FIXME: Another implementation pls
-	# RE; line 26- what do you need exactly? this normally shouldnt be called here. should be handled in the stats scene.
+
 	
 	var msg = ""
 	if stats:
