0171fa3f0b21af4f2fc0d53293dc8cc5766cdd29
AnidemDex <dav.medrin@gmail.com>
2021-01-05 01:03:01 -0500

    code cleanup
    reorganized audio files

--- src/collectables/money_bag/AudioStreamPlayer.gd
+++
@@ -1,6 +0,0 @@
-extends AudioStreamPlayer
-
-
-
-func _on_MoneyBag_area_entered(area):
-	_set_playing(true)
--- src/collectables/money_bag/money_bag.gd
+++ src/collectables/money_bag/money_bag.gd
@@ -3,4 +3,6 @@ extends Area2D
 
 func _on_MoneyBag_body_entered(body):
 	body.stats.sanity -= 1
+	$AudioStreamPlayer.play()
+	yield($AudioStreamPlayer, "finished")
 	queue_free()
