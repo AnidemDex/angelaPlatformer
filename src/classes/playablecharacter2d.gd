4bdee126b7738b8cda9c87d1f38beb8d0cc16fc1
AnidemDex <dav.medrin@gmail.com>
2021-01-05 18:42:31 -0500

    Added double jump.
    You can set the max of double jumps by the PlayableCharacter2D.max_jumps property

--- src/classes/playablecharacter2d.gd
+++ src/classes/playablecharacter2d.gd
@@ -10,6 +10,8 @@ var default_movement_speed:float
 var default_friction:float
 var default_acceleration:float
 var default_jump_force:float
+var jumps:int
+var can_jump:bool
 
 var _velocity:Vector2 = Vector2.ZERO
 var _speed:Vector2
@@ -19,19 +21,29 @@ export(float) var movement_speed = 150
 export (float, 0, 1.0) var friction = 0.1
 export (float, 0, 1.0) var acceleration = 0.25
 export(float) var jump_force = 200
+export(int)var max_jumps:int = 1
 
 export(String) var left_action = "ui_left"
 export(String) var right_action = "ui_right"
 export(String) var jump_action = "ui_up"
 
 func _ready():
-	
 	gravity = abs(gravity)
 	_assign_default_values()
 
 
 func _process(_delta):
 	_speed = Vector2(movement_speed, -jump_force)
+
+
+func _physics_process(_delta):
+	var is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	direction = _get_direction()
+	_velocity = _calculate_move_velocity(_velocity,direction,_speed,is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	
+	if is_on_floor():
+		jumps = max_jumps
 
 
 func restore_default_values() -> void:
@@ -42,24 +54,9 @@ func restore_default_values() -> void:
 	jump_force = default_jump_force
 
 
-func _assign_default_values() -> void:
-	default_gravity = gravity
-	default_movement_speed = movement_speed
-	default_friction = friction
-	default_acceleration = acceleration
-	default_jump_force = jump_force
-
-
-func _physics_process(_delta):
-	var is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0
-	direction = _get_direction()
-	_velocity = _calculate_move_velocity(_velocity,direction,_speed,is_jump_interrupted)
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
-
-
 func _get_direction() -> Vector2:
 	var x_axis = Input.get_action_strength(right_action)-Input.get_action_strength(left_action)
-	var y_axis = 1.0 if Input.is_action_just_pressed(jump_action) and is_on_floor() else 0.0
+	var y_axis = 1.0 if Input.is_action_just_pressed(jump_action) else 0.0
 	return Vector2(x_axis, y_axis)
 
 
@@ -76,12 +73,25 @@ func _calculate_move_velocity(
 		out.x = lerp(out.x, direction.x*speed.x, acceleration)
 	else:
 		out.x = lerp(out.x, 0, friction)
+	
 
 	
 	if direction.y:
-		out.y = (speed.y) * direction.y
+		if is_on_floor():
+			out.y = (speed.y) * direction.y
+		elif jumps > 0:
+			out.y = (speed.y) * direction.y
+			jumps -= 1
 	if is_jump_interrupted:
 		out.y = out.y/2
 	
 	out.y += gravity*get_physics_process_delta_time()
 	return out
+
+
+func _assign_default_values() -> void:
+	default_gravity = gravity
+	default_movement_speed = movement_speed
+	default_friction = friction
+	default_acceleration = acceleration
+	default_jump_force = jump_force
