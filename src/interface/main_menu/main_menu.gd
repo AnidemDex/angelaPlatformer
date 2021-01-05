4add0dada644d1220a8358a865630886fd9ce32a
AnidemDex <dav.medrin@gmail.com>
2021-01-05 14:09:49 -0500

    settings menu added
    music bars doesn't work yet

--- src/interface/main_menu/main_menu.gd
+++ src/interface/main_menu/main_menu.gd
@@ -3,6 +3,14 @@ extends Control
 signal quit
 signal replace_main_scene(with)
 
+onready var menu = $CanvasLayer/HBoxContainer/Center/Menu
+onready var settings_menu = $CanvasLayer/HBoxContainer/Center/SettingsNode
+
+onready var play_button = menu.get_node("PlayButton")
+
+func _ready():
+	play_button.grab_focus()
+
 
 func _on_PlayButton_pressed():
 	emit_signal("replace_main_scene","res://src/house/house.tscn")
@@ -12,5 +20,13 @@ func _on_QuitButton_pressed():
 	get_tree().quit()
 
 
-func _on_Settings_pressed():
+func _on_SettingsButton_pressed():
+	menu.hide()
+	settings_menu.show()
 	pass # Replace with function body.
+
+
+func _on_SettingsNode_ConfirmSection_pressed():
+	settings_menu.hide()
+	menu.show()
+	play_button.grab_focus()
