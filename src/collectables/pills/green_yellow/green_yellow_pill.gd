88fe8c2e907679b80b2514178f45cc9b15e720e0
AnidemDex <dav.medrin@gmail.com>
2021-01-05 02:19:51 -0500

    pill animation added
    hardcoded animations, bad practice

--- src/collectables/pills/green_yellow/green_yellow_pill.gd
+++ src/collectables/pills/green_yellow/green_yellow_pill.gd
@@ -2,9 +2,20 @@ extends Area2D
 
 onready var hurtbox:CollisionShape2D = $CollisionShape2D
 export(int) var temp_movement_speed = 0
+export(Color) var color = Color.green
 
 func _on_green_yellow_pill_body_entered(body):
 	body.movement_speed = temp_movement_speed
+	body.modulate = color
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(
+		body,
+		"modulate",
+		null,
+		Color.white,
+		5)
+	tween.start()
 	visible = false
 	hurtbox.set_deferred("disabled", true)
 	yield(get_tree().create_timer(5), "timeout")
--- src/collectables/pills/red_blue/red_blue_pill.gd
+++ src/collectables/pills/red_blue/red_blue_pill.gd
@@ -2,9 +2,22 @@ extends Area2D
 
 onready var hurtbox:CollisionShape2D = $CollisionShape2D
 export(int) var temp_jump_force = 0
+export(Color) var color = Color.cyan
 
 func _on_Pill_RedBlue_body_entered(body):
 	body.jump_force = temp_jump_force
+	
+	body.modulate = color
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(
+		body,
+		"modulate",
+		null,
+		Color.white,
+		10)
+	tween.start()
+	
 	visible = false
 	hurtbox.set_deferred("disabled", true)
 	yield(get_tree().create_timer(10), "timeout")
