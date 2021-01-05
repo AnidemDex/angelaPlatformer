8db352f4f895017f53eb7b974bd877086f6c5a3a
AnidemDex <dav.medrin@gmail.com>
2021-01-05 14:55:00 -0500

    Fixed some files and dependencies
    Some files was corrupted or without the right format

---
+++ src/levels/level2/Level 2.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+signal quit
+signal replace_main_scene(with)
--- src/levels/test_scene/test_scene.gd
+++ src/levels/test_scene/test_scene.gd
@@ -5,11 +5,6 @@ signal replace_main_scene(with)
 
 onready var narrator = $CanvasLayer/Narrator
 
-func _ready():
-	connect("replace_main_scene", self, "dev")
-
 func _on_Angela_request_text(text):
 	narrator.set_text_n_show(text)
 
-func dev(a):
-	print(a)
