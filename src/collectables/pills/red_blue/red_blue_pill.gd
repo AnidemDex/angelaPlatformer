226af8e8d5cca2dbfb62fbc4325c1a1849d3f358
landobot27 <46508791+landobot27@users.noreply.github.com>
2021-01-04 19:29:13 -0500

    pills work and there is a secret non functional collectible in the first level outside of the house
    basically the title

--- src/collectables/pills/red_blue/red_blue_pill.gd
+++ src/collectables/pills/red_blue/red_blue_pill.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
-
+onready var hurtbox:CollisionShape2D = $CollisionShape2D
+export(int) var temp_jump_force = 0
 
 func _on_Pill_RedBlue_body_entered(body):
-	queue_free()
+	body.jump_force = temp_jump_force
+	visible = false
+	hurtbox.set_deferred("disabled", true)
+	yield(get_tree().create_timer(10), "timeout")
+	body.jump_force = 200
+	visible = true
+	hurtbox.set_deferred("disabled", false)
