ab510ea99d6f838288141d350d4a82f9ce622a39
AnidemDex <dav.medrin@gmail.com>
2021-01-05 12:33:16 -0500

    Death zone node added
    Just be careful, it doesn't work if you play the scene alone

--- src/interface/transitions/transitions.gd
+++ src/interface/transitions/transitions.gd
@@ -1,13 +1,24 @@
+tool
+class_name Transition
 extends Node
 
 signal animation_finished(anim_name)
 
+enum TransitionAnim {
+	FADE_IN,
+	FADE_OUT,
+}
+
+var used_transition
+
 onready var animation = $AnimationPlayer
 
 func fade_in():
+	used_transition = TransitionAnim.FADE_IN
 	animation.play("fade_in")
 
 func fade_out():
+	used_transition = TransitionAnim.FADE_OUT
 	animation.play("fade_out")
 
 
--- src/main/main.gd
+++ src/main/main.gd
@@ -16,18 +16,35 @@ func go_to_main_menu():
 	var menu = load(main_menu)
 	replace_main_scene(menu)
 
+
 func go_to_level(level:String):
 	var scene = load(level)
 	replace_main_scene(scene)
 
-func replace_main_scene(resource:Resource):
-	print_debug("Changing main scene")
-	transition.fade_in()
+
+func replace_main_scene(resource:Resource, transition_type:int = 0):
+	print_debug("Replace main scene")
+	
+	make_transition_in(transition_type)
 	yield(transition, "animation_finished")
+	
 	if current_scene:
 		current_scene.visible = false
 
 	call_deferred("change_scene", resource)
+
+
+func make_transition_in(transition_type:int):
+	match transition_type:
+		Transition.TransitionAnim.FADE_IN:
+			transition.fade_in()
+
+
+func make_transition_out():
+	match transition.used_transition:
+		Transition.TransitionAnim.FADE_IN:
+			transition.fade_out()
+
 
 func change_scene(resource : Resource):
 
@@ -37,12 +54,13 @@ func change_scene(resource : Resource):
 
 	current_scene = resource.instance()
 	current_scene.visible = false
+	
 	current_scene.connect("ready", self, "_on_scene_ready")
 	current_scene.connect("quit", self, "go_to_main_menu")
 	current_scene.connect("replace_main_scene", self, "go_to_level")
-
+	
 	add_child(current_scene)
 
 func _on_scene_ready():
 	current_scene.visible = true
-	transition.fade_out()
+	make_transition_out()
