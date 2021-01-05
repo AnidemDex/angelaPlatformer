d37826e38a9047421e75a5dbdcd6a98eee938dc0
AnidemDex <dav.medrin@gmail.com>
2021-01-05 00:54:26 -0500

    code cleanup

--- src/levels/test_scene/test_scene.gd
+++ src/levels/test_scene/test_scene.gd
@@ -5,10 +5,6 @@ signal replace_main_scene(with)
 
 onready var narrator = $CanvasLayer/Narrator
 
-func _on_Angela2_died():
-	narrator.set_text_n_show("Angela probably died")
-
-
 func _on_Angela_request_text(text):
 	narrator.set_text_n_show(text)
 
