0e31117a986b65b81c3d3d1c386d38d390e17911
Llamadrama159 <76891803+Llamadrama159@users.noreply.github.com>
2021-01-08 10:18:19 -0800

    Update falling_ground.gd
    Fixed falling tiles

--- src/environment/falling_ground/falling_ground.gd
+++ src/environment/falling_ground/falling_ground.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
 export(float, 0.001, 1) var time_before_fall = 0.001
-export(int)var respawn_time = 2.5
+export(int)var respawn_time = 5
 
 var initial_position:Vector2 = Vector2.ZERO
 
@@ -11,17 +11,41 @@ func _physics_process(_delta):
 	if $RayCast2D.is_colliding():
 		$RayCast2D.enabled = false
 		_fall()
-
+	if $RayCast2D2.is_colliding():
+		$RayCast2D2.enabled = false
+		_fall2()
+	if $RayCast2D3.is_colliding():
+		$RayCast2D3.enabled = false
+		_fall3()
 
 func _fall():
 	($RayCast2D.get_collider() as PlayableBody2D).movement_speed -= 25
+	
 	yield(get_tree().create_timer(time_before_fall), "timeout")
 	mode = MODE_CHARACTER
 	yield(get_tree().create_timer(respawn_time), "timeout")
 	($RayCast2D.get_collider() as PlayableBody2D).movement_speed += 25
+
+func _fall2():
+	($RayCast2D2.get_collider() as PlayableBody2D).movement_speed -= 25
+	
+	yield(get_tree().create_timer(time_before_fall), "timeout")
+	mode = MODE_CHARACTER
+	yield(get_tree().create_timer(respawn_time), "timeout")
+	($RayCast2D2.get_collider() as PlayableBody2D).movement_speed += 25
+
+func _fall3():
+	($RayCast2D3.get_collider() as PlayableBody2D).movement_speed -= 25
+	
+	yield(get_tree().create_timer(time_before_fall), "timeout")
+	mode = MODE_CHARACTER
+	yield(get_tree().create_timer(respawn_time), "timeout")
+	($RayCast2D3.get_collider() as PlayableBody2D).movement_speed += 25
 
 func _on_VisibilityNotifier2D_screen_exited():
 	yield(get_tree().create_timer(respawn_time), "timeout")
 	mode = MODE_STATIC
 	position = initial_position
 	$RayCast2D.enabled = true
+	$RayCast2D2.enabled = true
+	$RayCast2D3.enabled = true
